9f046d07097b6055be716157b610f06c61265e38
waner1er <erwan@derniercri.io>
2022-08-22 09:32:17 +0200

    feature: responsive navbar

--- web/app/themes/wantheme/resources/scripts/app.js
+++ web/app/themes/wantheme/resources/scripts/app.js
@@ -3,22 +3,45 @@ import {domReady} from '@roots/sage/client';
 /**
  * app.main
  */
-const main = async (err) => {
-  if (err) {
-    // handle hmr errors
-    console.error(err);
-  }
+const main = async(err) => {
+    if (err) {
+      // handle hmr errors
+        console.error(err);
+    }
 
-  function toggleNavMenu() {
-    let toggle = document.querySelector('.nav-toggle'),
-      sidebar = document.querySelector('#menu-primary');
+    function toggleNavMenu()
+    {
+        let toggle = document.querySelector('.nav-toggle'),
+        sidebar = document.querySelector('#menu-primary');
+        let logo = document.querySelector('.r1-custom-logo');
 
-    toggle.addEventListener('click', function (e) {
-      e.preventDefault();
-      sidebar.classList.toggle('hidden');
-    });
-  }
-  toggleNavMenu();
+        if (window.innerWidth <= 768) {
+          logo.classList.add('hidden');
+        }
+
+        toggle.addEventListener('click', function (e) {
+            e.preventDefault();
+            if (sidebar.classList.contains('hidden')) {
+                sidebar.classList.remove('hidden');
+                logo.classList.remove('hidden');
+            } else {
+                sidebar.classList.add('hidden');
+                logo.classList.add('hidden');
+            }
+        });
+        window.onresize = function () {
+            if (window.innerWidth <= 768) {
+                sidebar.classList.add('hidden');
+                logo.classList.add('hidden');
+            } else {
+                sidebar.classList.add('hidden');
+                logo.classList.remove('hidden');
+            }
+        }
+    }
+
+    toggleNavMenu();
+
 };
 
 /**
