a8de6200ebd97b524b302e3fedff1b6a2fbaaca1
waner1er <erwan@derniercri.io>
2022-08-23 17:56:40 +0200

    updating code

--- web/app/themes/wantheme/resources/views/single.blade.php
+++ web/app/themes/wantheme/resources/views/single.blade.php
@@ -3,9 +3,8 @@
 @section('content')
   @while(have_posts())
     @php(the_post())
-    <div style="font-size: 40px">commentaires : {{ comments_number() }}</div>
+    Categories = {{ $tax->name ?? 'no category '}}
 
-    Categories = {{ $tax->name ?? ''}}
     @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
   @endwhile
 @endsection
